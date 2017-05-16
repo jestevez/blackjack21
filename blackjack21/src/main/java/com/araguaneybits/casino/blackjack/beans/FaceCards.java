21f3e5e9a651cac770e8193a13ac5fe3d3d03c58
jestevez <jestevez@yeyo>
2017-05-16 22:59:56 +0200

    Enumeracion con los valores de las cartas

---
+++ blackjack21/src/main/java/com/araguaneybits/casino/blackjack/beans/FaceCards.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 AraguaneyBits
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.araguaneybits.casino.blackjack.beans;
+
+/**
+ *
+ * @author jestevez
+ */
+public enum FaceCards {
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    J("J", 10),
+    Q("Q", 10),
+    K("K", 10),
+    A("A", 11);
+
+    private final String rank;
+    private final int value;
+
+    private FaceCards(String rank, int value) {
+        this.rank = rank;
+        this.value = value;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
