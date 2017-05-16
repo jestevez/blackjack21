5457fd65dd93c1b6c3b97a84ad39aed9697d4e96
jestevez <jestevez@yeyo>
2017-05-16 22:58:42 +0200

    Clase Java que representa una carta

---
+++ blackjack21/src/main/java/com/araguaneybits/casino/blackjack/beans/Card.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 AraguaneyBits
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.araguaneybits.casino.blackjack.beans;
+
+/**
+ *
+ * @author jestevez
+ */
+public class Card {
+
+    private String rank;
+    private String suit;
+    private int value;
+    private int sort;
+
+    @Override
+    public String toString() {
+        return rank + " " + suit + " " + value;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getSort() {
+        return sort;
+    }
+
+    public void setSort(int sort) {
+        this.sort = sort;
+    }
+
+}
